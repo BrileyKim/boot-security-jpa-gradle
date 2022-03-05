b46d3e0071a17a1cb487f47b8f69ff1b3a3df970
이성준 <tjdwns54522@gmail.com>
2022-03-05 22:32:40 +0900

    ggggg

--- boot-security-jpa-gradle-1/src/main/java/com/example/demo/lsj/controller/LsjController.java
+++ boot-security-jpa-gradle-1/src/main/java/com/example/demo/lsj/controller/LsjController.java
@@ -16,7 +16,7 @@ public class LsjController {
 	public ModelAndView SelectList(ModelAndView mav) {
 		
 		mav.setViewName("/lsj/list");
-		System.out.println("ffffff");
+		System.out.println("gggggggg");
 		
 		return mav;
 		
