a30f23a1801dff98699f18e0c880cf01851248d4
이성준 <devSjLee>
2022-03-05 22:18:54 +0900

    gdgd

--- boot-security-jpa-gradle-1/src/main/java/com/example/demo/lsj/controller/LsjController.java
+++ boot-security-jpa-gradle-1/src/main/java/com/example/demo/lsj/controller/LsjController.java
@@ -16,7 +16,7 @@ public class LsjController {
 	public ModelAndView SelectList(ModelAndView mav) {
 		
 		mav.setViewName("/lsj/list");
-		System.out.println("gdgdgd");
+		System.out.println("ddddddd");
 		
 		return mav;
 		
