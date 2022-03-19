8ac0648ba70eeb25abbf40b66ac5842bcf054f88
ganam <briley.kim.lee@gmail.com>
2022-03-19 22:40:06 +0900

    [김가남] JSON 타입으로 데이터 제공하는 API로 바꿔서 가져오기 성공

--- boot-security-jpa-gradle-1/src/main/java/com/example/demo/kgn/controller/KgnController.java
+++ boot-security-jpa-gradle-1/src/main/java/com/example/demo/kgn/controller/KgnController.java
@@ -31,21 +31,6 @@ public class KgnController {
 	
 	private final KgnService kgnService;
 	
-	private String encodeValue(String value) {
-		
-		String returnValue = "";
-		
-		try {
-			returnValue = URLEncoder.encode(value,
-					StandardCharsets.UTF_8.toString());
-			
-		}catch(UnsupportedEncodingException uee) {
-			uee.printStackTrace();
-		}
-		return returnValue;
-		
-	}
-	
 	@GetMapping()
 	public ModelAndView SelectList(ModelAndView mav) throws UnsupportedEncodingException{
 		
@@ -53,63 +38,31 @@ public class KgnController {
 		try {
 			
 			
-			Map<String,String> requestParams = new LinkedHashMap<String,String>();
-			requestParams.put("serviceKey","bYhQpkws1NzgWD6fE5nIDq%2B%2FlsRs4i9gnV0Y6VGGqP8D7uqWuiN3JDXrO7%2F%2Byx%2Bciv8hJYfL%2FcE%2BsbIiIypjXQ%3D%3D");
-			requestParams.put("pageNo","1");
-			requestParams.put("numOfRows","10");
-			
-		
-			String encodedURL = requestParams.keySet().stream()
-				      .map(key -> key + "=" + encodeValue(requestParams.get(key)))
-				      .collect(Collectors.joining("&", "http://apis.data.go.kr/B551182/rprtHospService?", ""));
-			
-			
-			//1. URL을 만들기 위한 StringBuilder
-			StringBuilder urlBuilder = new StringBuilder("http://apis.data.go.kr/B551182/rprtHospService");
-			
-			//2. 오픈 API 요청 규격에 맞는 파라미터 생성, 발급받은 인증키
-			urlBuilder.append("?"+URLEncoder.encode("serviceKey","UTF-8")+"=bYhQpkws1NzgWD6fE5nIDq%2B%2FlsRs4i9gnV0Y6VGGqP8D7uqWuiN3JDXrO7%2F%2Byx%2Bciv8hJYfL%2FcE%2BsbIiIypjXQ%3D%3D");
-			urlBuilder.append("&"+URLEncoder.encode("pageNo","UTF-8")+"="+URLEncoder.encode("1","UTF-8"));
-			urlBuilder.append("&"+URLEncoder.encode("numOfRows","UTF-8")+"="+URLEncoder.encode("10","UTF-8"));
-			
-			System.out.println("1 : "+urlBuilder.toString());
-			System.out.println("2 : "+StringEscapeUtils.escapeJava(urlBuilder.toString()));
-			System.out.println("3 : "+encodedURL);
-			
-			//3.URL 객체 생성
-			URL url = new URL(encodedURL);
-			
-			//4.요청하고자 하는 URL과 통신하기 위한 Connection 객체 생성
-			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-			
-			//5.통신을 위한 메소드 SET
-			conn.setRequestMethod("GET");
-			
-			//6.통신을 위한 Content-type SET
-			conn.setRequestProperty("Content-type", "application/json");
-			
-			//7.통신 응답 코드 확인 
-			System.out.println("Response code: "+conn.getResponseCode());
-			
-			// 8. 전달받은 데이터를 BufferedReader 객체로 저장.
+	        StringBuilder urlBuilder = new StringBuilder("http://apis.data.go.kr/6440000/firePlug/getFrplgCnJSONDataForAPI"); /*URL*/
+	        urlBuilder.append("?" + URLEncoder.encode("serviceKey","UTF-8") + "=bYhQpkws1NzgWD6fE5nIDq%2B%2FlsRs4i9gnV0Y6VGGqP8D7uqWuiN3JDXrO7%2F%2Byx%2Bciv8hJYfL%2FcE%2BsbIiIypjXQ%3D%3D"); /*Service Key*/
+	        urlBuilder.append("&" + URLEncoder.encode("searchCondition","UTF-8") + "=" + URLEncoder.encode("1", "UTF-8")); /*검색어 구분(1=주소, 2=시설번호, 3=시군명)*/
+	        urlBuilder.append("&" + URLEncoder.encode("searchKeyword","UTF-8") + "=" + URLEncoder.encode("홍성군", "UTF-8")); /*검색키워드*/
+	        urlBuilder.append("&" + URLEncoder.encode("searchSigun","UTF-8") + "=" + URLEncoder.encode("44800", "UTF-8")); /*행정표준코드*/
+	        urlBuilder.append("&" + URLEncoder.encode("searchFacltCd","UTF-8") + "=" + URLEncoder.encode("01", "UTF-8")); /*소화전구분(01 = 지상, 02 = 지하, 03 = 급수탑, 04 = 저수조)*/
+	        URL url = new URL(urlBuilder.toString());
+	        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+	        conn.setRequestMethod("GET");
+	        conn.setRequestProperty("Content-type", "application/json");
+	        
 	        BufferedReader rd;
 	        if(conn.getResponseCode() >= 200 && conn.getResponseCode() <= 300) {
 	            rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
 	        } else {
 	            rd = new BufferedReader(new InputStreamReader(conn.getErrorStream()));
 	        }
-	        // 9. 저장된 데이터를 라인별로 읽어 StringBuilder 객체로 저장.
 	        StringBuilder sb = new StringBuilder();
 	        String line;
 	        while ((line = rd.readLine()) != null) {
 	            sb.append(line);
 	        }
-	        // 10. 객체 해제.
 	        rd.close();
 	        conn.disconnect();
-	        // 11. 전달받은 데이터 확인.
-	        System.out.println(sb.toString());
-			
+	        
 		
 		} catch (UnsupportedEncodingException e) {
 			e.printStackTrace();
